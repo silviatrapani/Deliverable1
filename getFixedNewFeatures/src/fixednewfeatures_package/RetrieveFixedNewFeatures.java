1a5bdd3061e129cf8a03433855630b9eb749455d
silviatrapani <80124333+silviatrapani@users.noreply.github.com>
2021-03-27 19:46:59 +0100

    Modifica della stampa nel file csv.

--- getFixedNewFeatures/src/fixednewfeatures_package/RetrieveFixedNewFeatures.java
+++ getFixedNewFeatures/src/fixednewfeatures_package/RetrieveFixedNewFeatures.java
@@ -15,6 +15,8 @@ import java.util.Map;
 import java.util.Collections;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,7 +26,7 @@ import java.util.logging.Logger;
 public class RetrieveFixedNewFeatures {
 	
    public static final List<LocalDateTime> featuresFixed = new ArrayList<LocalDateTime>();
-   protected static final Map<LocalDateTime, String> issueKey = new HashMap<> ();
+   protected static final Map<String,LocalDateTime> issueKey = new HashMap<> ();
 
    private static String readAll(Reader rd) throws IOException {
 	      StringBuilder sb = new StringBuilder();
@@ -52,67 +54,72 @@ public class RetrieveFixedNewFeatures {
           return new JSONObject(readAll(rd));
       }
    }
+      
    
+  
    public static void main(String[] args) throws IOException, JSONException {
 	   
 	   String projName ="TAJO";
 	   Integer j = 0;
 	   Integer i = 0;
 	   Integer total = 1;
-	
-do {
-	 //Get JSON API for fixed new features w/ AV in the project
+	   //Get JSON API for fixed new features w/ AV in the project
 	   j = i + 1000;
 	   String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
                  + projName + "%22AND%22issueType%22=%22"+"New+Feature"+"%22AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
                  + i.toString() + "&maxResults=" + j.toString();
-  JSONObject json = readJsonFromUrl(url);
-  JSONArray issues = json.getJSONArray("issues");
+	   JSONObject json = readJsonFromUrl(url);
+	   JSONArray issues = json.getJSONArray("issues");
   
-  total = json.getInt("total");
-  for (; i < total && i<j; i++ ) {
-     	 JSONObject obj = issues.getJSONObject(i%1000);
+	   for (i=0; i < issues.length(); i++ ) {
+     	 JSONObject obj = issues.getJSONObject(i);
      	 String key = obj.get("key").toString();
      	 JSONObject fields = obj.getJSONObject("fields");
      	 String resolutiondate = (String)fields.getString("resolutiondate");
      	 String date = resolutiondate.split("T")[0];
-     	 addRelease(date,key);
+     	 addNewFeature(date,key);
+     	
 	   }
-} while (i < total);
- 
-
-
-
-// sort fixdates
-Collections.sort(featuresFixed,(LocalDateTime o1, LocalDateTime o2) -> -o1.compareTo(o2));  
-
-
-try(FileWriter fileWriter = new FileWriter(projName + "FixedDatesTAJO.csv")) {
-	fileWriter.append("Index,Key,Date");
-    fileWriter.append("\n");
-    for ( i = 0; i < featuresFixed.size(); i++) {
-       Integer index = i + 1;
-       fileWriter.append(index.toString());
-       fileWriter.append(",");
-       fileWriter.append(issueKey.get(featuresFixed.get(i)));
-       fileWriter.append(",");
-       fileWriter.append(featuresFixed.get(i).toString());
-       fileWriter.append("\n");
-       fileWriter.flush();
-    }
-} catch (Exception e) {
-	final Logger log = Logger.getLogger(RetrieveFixedNewFeatures.class.getName());
-	log.info("Error in csv writer");
-    e.printStackTrace();
- }
-
-public static void addRelease(String strDate, String id) {
-		 LocalDate date = LocalDate.parse(strDate);
-     LocalDateTime dateTime = date.atStartOfDay();
-        featuresFixed.add(dateTime);
-     issueKey.put(dateTime, id);
-     return;
- }
-	 }
+	   
+	   
+	   Collections.sort(featuresFixed,(LocalDateTime o1, LocalDateTime o2) -> o1.compareTo(o2));  
+		
+		
+		try(FileWriter fileWriter = new FileWriter(projName + "FixedDatesTAJO.csv")) {
+			fileWriter.append("Index,Key,Date");
+	        fileWriter.append("\n");
+	        for ( i = 0; i < issueKey.size(); i++) {
+	           Integer index = i + 1;
+	           fileWriter.append(index.toString());
+	           fileWriter.append(",");
+	           fileWriter.append((String) issueKey.keySet().toArray()[i]);
+	           fileWriter.append(",");
+	           LocalDateTime date = issueKey.get(issueKey.keySet().toArray()[i]);
+	           DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	           String formatDateTime = date.format(formatter);
+	           fileWriter.append(formatDateTime);
+	           fileWriter.append("\n");
+	           fileWriter.flush();
+	        }
+		} 
+		catch (Exception e) {
+			final Logger log = Logger.getLogger(RetrieveFixedNewFeatures.class.getName());
+	    	log.info("Error in csv writer");
+	        e.printStackTrace();
+	     }
+		
+		
+		
+		 public static void addNewFeature(String strDate, String id) {
+	  		 LocalDate date = LocalDate.parse(strDate);
+		     LocalDateTime dateTime = date.atStartOfDay();
+		     issueKey.put(id, dateTime);
+		     if (!featuresFixed.contains(dateTime))
+		         featuresFixed.add(dateTime);
+		     
+		      return;
+		  }
+	  	 
+	  }
 
 
